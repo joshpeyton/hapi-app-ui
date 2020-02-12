d76b51f2fda05e2a47acff66aaddb63670903d92
Josh Peyton <jpeyton04@yahoo.com>
2020-02-12 11:32:24 -0500

    cleanup labrc and gitignore

--- .labrc.js
+++ .labrc.js
@@ -1,20 +1,10 @@
 'use strict';
 
-const internals = {};
-
-internals.reporters = new Map([
-    ['html', 'coverage.html'],
-    ['junit', 'junit.xml'],
-    ['clover', 'clover.xml'],
-    ['console', 'stdout']
-]);
-
-const reporters = Array.from(internals.reporters.keys());
-const reportersOutput = Array.from(internals.reporters.values());
-
-module.exports.coverage = true;
-module.exports.threshold = 100;
-module.exports.lint = true;
-module.exports.reporter = reporters;
-module.exports.output = reportersOutput;
-module.exports["coverage-exclude"] = ["client", "dist", "bower_components", "temp"];
+module.exports = {
+    coverage: true,
+    threshold: 100,
+    lint: true,
+    ["coverage-exclude"]: ["client", "dist", "temp"],
+    reporter: ['console', 'html'],
+    output: ['stdout', 'coverage.html']
+};
